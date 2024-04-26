9df5d6e92708793d19c602d3104f2090a4a6e58a
hoanganh2321 <131464214+hoanganh2321@users.noreply.github.com>
2024-04-26 18:00:03 +0700

    Add files via upload

---
+++ arceus.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+import requests
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+    print("==========================")
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Coder Kaioshin 🔥"))
+
+@bot.command(name="key", description="Bypass arceusx Key For Your")
+async def fluxus(ctx, link: str):
+    hwid = link.split("hwid=")[-1].split("&")[0]  
+    
+    url = f"https://stickx.top/api-arceusx/?hwid={hwid}&api_key=E99l9NOctud3vmu6bPne"
+    
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        await ctx.reply(f"> # _{data['key']}_")
+        await ctx.reply(f"> # _Dino Óc CHó Vcl !_")
+    else:
+        print(f"Failed to retrieve data from API with status code {response.status_code}")
+        await ctx.reply(f"Failed to retrieve data from API with status code {response.status_code}") 
+
+bot.run('TOKEN')
