fdc13a5347f592dd010477d816881c37fee799ea
hoanganh2321 <131464214+hoanganh2321@users.noreply.github.com>
2024-04-26 19:49:19 +0700

    Update arceus.py

--- arceus.py
+++ arceus.py
@@ -1,29 +1,62 @@
 import discord
-from discord.ext import commands
 import requests
+import datetime
+from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+intents = discord.Intents.all()
+intents.members = True
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}")
-    print("==========================")
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Coder Kaioshin 🔥"))
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="Kaioshin Dev🔥"))
+    print(f"Đăng nhập với tên {bot.user}")
 
-@bot.command(name="key", description="Bypass arceusx Key For Your")
-async def fluxus(ctx, link: str):
-    hwid = link.split("hwid=")[-1].split("&")[0]  
-    
-    url = f"https://stickx.top/api-arceusx/?hwid={hwid}&api_key=E99l9NOctud3vmu6bPne"
-    
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        data = response.json()
-        await ctx.reply(f"> # _{data['key']}_")
-        await ctx.reply(f"> # _Dino Óc CHó Vcl !_")
+allowed_channels = [1231100851347525692]
+
+@bot.event
+async def on_message(message):
+    if message.author != bot.user and message.content.startswith("https://spdmteam.com/key-system-1?hwid="):
+        if message.channel.id not in allowed_channels:
+            return
+
+        start_time = datetime.datetime.now()
+        api = 'https://stickx.top/api-arceusx/?hwid='
+        api_key = 'E99l9NOctud3vmu6bPne'
+        hwidd = message.content.replace("https://spdmteam.com/key-system-1?hwid=", "")
+        hwid = hwidd.replace("&zone=Europe/Rome&os=android", "")
+
+        wait_embed = discord.Embed(title="Bypassing...! ⏳", description="Vui Lòng Chờ Trong Giây Lát", color=0xffff00)
+        wait_embed.timestamp = datetime.datetime.now(datetime.timezone.utc)
+        wait_message = await message.reply(embed=wait_embed)
+
+        response = requests.get(f"{api}{hwid}&api_key={api_key}")
+
+        json_data = response.json()
+        key = json_data.get("key")
+
+        if key == "Invalid HWID":
+            embed = discord.Embed(title="Lỗi ❌", description="```Link Sai```", color=0xFF0000)
+            embed.add_field(name="Your Link:", value=f"```\n{message.content}\n```", inline=False)
+        else:
+            embed = discord.Embed(title="Status:", description=f"```{key}```", color=0x00ff00)
+            embed.add_field(name="Your Link:", value=f"```\n{message.content}\n```", inline=False)
+            embed.set_thumbnail(url="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdml5dDVidHdvcjJ6Njdza2N6eHJnODMxaHIwZWI2emN3aXo0ejNwaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/wkW0maGDN1eSc/giphy.gif")
+            embed.set_footer(text=f"| Bot by Kaioshin |")
+            embed.timestamp = datetime.datetime.now(datetime.timezone.utc)
+            print(key)
+
+        await wait_message.edit(embed=embed)
+
+    await bot.process_commands(message)
+
+@bot.command()
+async def setchannel(ctx):
+    if ctx.author.guild_permissions.administrator:
+        allowed_channels.append(ctx.channel.id)
+        await ctx.send("# This channel is now set for bypass.")
     else:
-        print(f"Failed to retrieve data from API with status code {response.status_code}")
-        await ctx.reply(f"Failed to retrieve data from API with status code {response.status_code}") 
+        await ctx.send("# Only Admin can use.")
 
 bot.run('TOKEN')
